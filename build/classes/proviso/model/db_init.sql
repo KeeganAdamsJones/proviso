b6d4d47aeb45bedd4f4d75bd5416378710a5b13e
KeeganAdamsJones <73457258+KeeganAdamsJones@users.noreply.github.com>
2021-11-16 14:10:24 -0600

    added db script, landing page, css file, logo and landing page image

---
+++ build/classes/proviso/model/db_init.sql
@@ -0,0 +1,53 @@
+/*
+    db_init.sql
+    Keegan Jones
+    11/16/2021
+    Proviso MySQL DB initialization to create tables
+*/
+
+-- drop test user if exists 
+DROP USER IF EXISTS 'proviso_user'@'localhost';
+
+
+-- create proviso_user and grant them all privileges to the proviso_user database 
+CREATE USER 'proviso_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'MySQL5IsGreat!';
+
+
+-- grant all privileges to the proviso database to user proviso_user on localhost 
+GRANT ALL PRIVILEGES ON proviso.* TO'proviso_user'@'localhost';
+
+
+-- Drop the foreign key
+ALTER TABLE reservation DROP FOREIGN KEY fk_customer;
+
+
+-- drop tables if they are present
+DROP TABLE IF EXISTS customer; 
+DROP TABLE IF EXISTS reservation; 
+
+
+-- create customer table 
+CREATE TABLE customer (
+	customer_id 		INT 			NOT NULL		AUTO_INCREMENT,
+	first_name		VARCHAR(75)		NOT NULL,
+	last_name		VARCHAR(75)		NOT NULL,
+	user_name		VARCHAR(75)		NOT NULL;
+	password		VARCHAR(75)		NOT NULL;
+	PRIMARY KEY(customer_id)
+);
+
+-- create reservation table 
+CREATE TABLE reservation (
+	reservation_id		INT 			NOT NULL 		AUTO_INCREMENT,
+	roomSize			Varchar(150)	Not Null, 
+	customer_id			INT				NOT NULL,
+	amenities			VARCHAR(75)		NOT NULL,
+	guests				INT				NOT NULL,
+	loyaltyPoints		INT(150)		NOT NULL,
+	
+	PRIMARY KEY(reservation_id),
+	CONSTRAINT fk_customer
+	FOREIGN KEY(customer_id)
+		REFERENCES customer(customer_id)
+); 
+
---
+++ src/main/webapp/jsp/index.jsp
@@ -0,0 +1,57 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<!-- 
+Keegan Jones
+CSD - 460 Capstone
+Poviso
+11/16/2021
+ -->
+
+<!--  
+	Image references 
+	hotel bedroom image: https://www.usatoday.com/picture-gallery/travel/hotels/2020/05/18/coronavirus-how-transform-your-bedroom-into-luxury-hotel-suite/5214698002/
+	
+-->
+
+
+<!DOCTYPE html>
+
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<title>Proviso</title>
+	</head>
+	
+	<link
+		rel="stylesheet" href="site.css">
+			
+	
+	<body>
+		
+			<div class='center'>
+				<img src="images/logo.jpeg" alt="Proviso Logo" width="200" height="148">
+			</div>
+		<!-- Tie in the TopNav file and where I want it.
+		<jsp:include page="TopNav.jsp" flush="true" /> -->
+	
+		<main>
+				<p class="center">Book your stay in our first class luxury suites today!
+				</p>
+				
+				<div class='center'>
+					<img src="images/hotelRoom.jpeg" alt="hotel room photo" width="600" >
+				</div>
+		
+		
+	
+		
+		</main>
+		
+		<!-- Page footer
+		<jsp:include page="Footer.jsp" flush="true" />
+		 -->
+		 
+	
+	</body>
+</html>
